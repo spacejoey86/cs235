0ace22030e4e0e27ffa6896dfba7d86bc1ed9b52
Dylan Gardham <message4dylan@gmail.com>
2024-02-15 08:16:38 +0000

    make direction flipping static and in the direction enum file

--- src/main/java/swan/g09/cs230a2/Direction.java
+++ src/main/java/swan/g09/cs230a2/Direction.java
@@ -13,5 +13,26 @@ public enum Direction {
     /** The South movement direction. */
     SOUTH,
     /** The West movement direction. */
-    WEST
+    WEST;
+
+    /**
+     * flip of the direction
+     * @return the flipped direction
+     */
+    private Direction flip;
+
+    static {
+        NORTH.flip = SOUTH;
+        SOUTH.flip = NORTH;
+        EAST.flip = WEST;
+        WEST.flip = EAST;
+    }
+
+    /**
+     * gets the opposite direction of the current direction
+     * @return the opposite direction
+     */
+    public Direction flip() {
+        return flip;
+    }
 };
